730a3186de952b16f99d849841257ebb90dcb69e
SarveshShirodkar27 <shirodkarsarvesh999@gmail.com>
2025-09-04 11:19:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+       function showSection(sectionId) {
+            // Hide all sections
+            const sections = document.querySelectorAll('.section');
+            sections.forEach(section => {
+                section.classList.remove('active');
+            });
+
+            // Show the selected section
+            const targetSection = document.getElementById(sectionId);
+            if (targetSection) {
+                targetSection.classList.add('active');
+            }
+        }
+        showSection('home');
